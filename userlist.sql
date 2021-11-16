2cf83dafd1002a6fcc1d79f99efe395292b224b2
NoroIso <norayrisahakyan@gmail.com>
2021-11-16 22:36:03 +0400

    fourth commit

--- userlist.sql
+++ userlist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2021 at 10:04 AM
+-- Generation Time: Nov 16, 2021 at 07:34 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -81,15 +81,15 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(288, '2014_10_12_000000_create_users_table', 1),
-(289, '2014_10_12_100000_create_password_resets_table', 1),
-(290, '2014_10_12_200000_add_two_factor_columns_to_users_table', 1),
-(291, '2019_08_19_000000_create_failed_jobs_table', 1),
-(292, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(293, '2021_11_06_195400_create_roles_table', 1),
-(294, '2021_11_07_164431_create_role_user_table', 1),
-(295, '2021_11_09_080059_create_posts_table', 1),
-(296, '2021_11_09_080455_create_post_user_table', 1);
+(166, '2014_10_12_000000_create_users_table', 1),
+(167, '2014_10_12_100000_create_password_resets_table', 1),
+(168, '2014_10_12_200000_add_two_factor_columns_to_users_table', 1),
+(169, '2019_08_19_000000_create_failed_jobs_table', 1),
+(170, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(171, '2021_11_06_195400_create_roles_table', 1),
+(172, '2021_11_07_164431_create_role_user_table', 1),
+(173, '2021_11_09_080059_create_posts_table', 1),
+(174, '2021_11_09_080455_create_post_user_table', 1);
 
 -- --------------------------------------------------------
 
@@ -102,13 +102,6 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `password_resets`
---
-
-INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
-('y@test.com', '$2y$10$jajP52Nd9yQRnWlYsnAsk..39/wBNa72kW7GnVKZQWerjExrxTfZy', '2021-11-13 06:07:31');
 
 -- --------------------------------------------------------
 
@@ -138,6 +131,7 @@ CREATE TABLE `posts` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -146,17 +140,18 @@ CREATE TABLE `posts` (
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Corrupti quia dolorem facere temporibus eos quae.', 'Architecto rerum quo commodi dolores hic atque. Repellat odit qui pariatur mollitia adipisci. Distinctio eos placeat minus optio quia vel est.', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(2, 'Voluptate earum voluptatum inventore earum dignissimos.', 'Nihil officia iste et consequatur distinctio. Molestiae minima et nostrum sit. Praesentium et eos deleniti cum molestiae', '2021-11-13 04:36:25', '2021-11-13 05:06:21'),
-(3, 'Necessitatibus aut voluptas rerum.', 'Sit ratione minus consequatur et laudantium. Et incidunt fugit adipisci molestiae atque. Velit ut quibusdam omnis laborum. Et qui ut id doloribus quam sit qui. Quia sit et quae sunt porro.', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(4, 'Enim impedit et nam voluptatem optio.', 'Quis eligendi consequatur perferendis porro. Ullam pariatur aliquid ratione ut ipsa cumque. Omnis qui sit ipsa omnis.', '2021-11-13 04:36:26', '2021-11-13 04:36:26'),
-(5, 'Excepturi commodi repellat iste quas ea in sed assumenda.', 'Doloremque laborum atque nihil omnis. Ex ullam neque ut beatae assumenda porro. Aut sunt sunt quam ut perspiciatis. Vero dolorum pariatur ut.', '2021-11-13 04:36:26', '2021-11-13 04:36:26'),
-(6, 'Laboriosam nesciunt est aut et facilis.', 'Qui voluptatibus illo est et. Sequi facilis voluptas explicabo accusantium placeat totam ratione. Ducimus et et accusamus nemo velit.', '2021-11-13 04:36:26', '2021-11-13 04:36:26'),
-(7, 'Et ut dicta et reiciendis.', 'Quia expedita tempore nihil reprehenderit voluptas dolor voluptas. Aut animi corporis dolor et doloribus. Facilis eum suscipit doloribus exercitationem in qui explicabo adipisci. Ea officia est qui voluptatem adipisci et.', '2021-11-13 04:36:26', '2021-11-13 04:36:26'),
-(8, 'Pariatur dolor sit omnis dolorum fugit.', 'Sapiente a cum voluptatum nobis nam non quos. Sint officia sed corporis reiciendis illo perferendis tempore.', '2021-11-13 04:36:26', '2021-11-13 04:36:26'),
-(9, 'Rerum qui soluta sunt et et labore.', 'Iste consequatur nobis quia reiciendis maxime quia. Doloremque saepe soluta dolor et. In harum architecto et tempora quae velit.', '2021-11-13 04:36:26', '2021-11-15 03:00:20'),
-(10, 'Aspernatur quia nulla et nam.', 'Ut labore dolorem non iste. Asperiores asperiores fugit adipisci quis itaque ipsa.', '2021-11-13 04:36:26', '2021-11-13 05:13:25');
+INSERT INTO `posts` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Nisi voluptates reprehenderit ipsum nostrum rem natus pariatur.', 'Distinctio unde qui neque vel repudiandae excepturi velit. Non amet omnis et suscipit. Consectetur quasi odit exercitationem aut praesentium eum iusto. Sunt dolorem libero sint rem ea.', 'https://via.placeholder.com/640x480.png/004455?text=doloremque', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(2, 'Dignissimos atque maiores voluptatibus ea fugiat sunt.', 'Ut exercitationem et repellendus veritatis aut. Est eveniet sit fuga consectetur id consequatur. Sunt ab sit mollitia amet pariatur eligendi culpa. Velit atque excepturi ex sunt rerum quia.', 'https://via.placeholder.com/640x480.png/00cccc?text=eos', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(3, 'Est incidunt nulla nulla.', 'Omnis ut ex dicta est cumque. Consequatur fuga ad voluptatem quaerat ipsum. Laborum beatae earum dolor adipisci. Enim odio aut ut deleniti.', 'https://via.placeholder.com/640x480.png/00aaee?text=a', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(4, 'Ab repellendus aspernatur est rem unde aut veniam.', 'Vel non velit quas et consectetur in at omnis. Est quo laudantium est veniam. Id est aliquam voluptatem repudiandae nam est ea aut. Unde provident dolores suscipit consequatur sunt delectus perferendis aut.', 'https://via.placeholder.com/640x480.png/003311?text=sint', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(5, 'Ex ut molestiae hic autem qui dolores temporibus illo.', 'Vero pariatur laborum repellendus veritatis et et doloribus. Aut quo voluptas dolore deleniti ullam. Repellendus neque quidem minima accusamus. Adipisci expedita nesciunt labore est illum unde perferendis.', 'https://via.placeholder.com/640x480.png/009933?text=quibusdam', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(6, 'Error non odio minus vitae exercitationem excepturi illum voluptas.', 'Rerum distinctio tenetur accusamus recusandae alias. Inventore exercitationem accusamus voluptate consequatur recusandae. Maxime saepe est quo qui. Sunt autem et ipsum deserunt molestiae autem.', 'https://via.placeholder.com/640x480.png/00aaff?text=quia', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(8, 'Ut officia est repudiandae nostrum sequi atque.', 'Recusandae distinctio maiores dignissimos ab vero nemo rerum. Molestiae nemo praesentium quis qui distinctio atque omnis laboriosam. Architecto exercitationem perspiciatis ut non dolore. Ut quidem est quod eum.', 'https://via.placeholder.com/640x480.png/0088ee?text=eum', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(9, 'Debitis accusamus totam at vel soluta totam aspernatur.', 'Reprehenderit maxime accusamus repellendus ipsam ut voluptatem eum numquam. Pariatur velit at laudantium ipsa repudiandae consequuntur rem. Ducimus porro incidunt rerum aut.', 'https://via.placeholder.com/640x480.png/0022cc?text=odio', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(10, 'Sunt voluptas quidem similique non officiis.', 'Ipsum reiciendis provident voluptas reprehenderit sed. Dolores atque expedita iusto sunt molestiae occaecati. Assumenda minima numquam consequatur eaque maxime cumque odit nesciunt.', 'https://via.placeholder.com/640x480.png/006666?text=est', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(11, 'dfrgthygtnfredmswswmkdefrngt', 'qwertyuioiuytrewawergtyhjukiolikjuyhgtrfdeswdefrgthyjukiolkijuhygtfredwsdefrgthyjuikjuhygtfredfrgthyjujhygtfrdes', 'C:\\xampp\\xampp\\tmp\\phpAC8C.tmp', '2021-11-16 14:26:36', '2021-11-16 14:26:36'),
+(12, '4610215155', '12345679984511515111448', 'C:\\xampp\\xampp\\tmp\\phpF984.tmp', '2021-11-16 14:26:56', '2021-11-16 14:26:56');
 
 -- --------------------------------------------------------
 
@@ -177,11 +172,15 @@ CREATE TABLE `post_user` (
 --
 
 INSERT INTO `post_user` (`id`, `post_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 5, 1, NULL, NULL),
-(2, 8, 2, NULL, NULL),
-(4, 7, 4, NULL, NULL),
-(5, 8, 5, NULL, NULL),
-(6, 6, 6, NULL, NULL);
+(1, 9, 1, NULL, NULL),
+(2, 10, 2, NULL, NULL),
+(4, 9, 4, NULL, NULL),
+(5, 5, 5, NULL, NULL),
+(6, 8, 6, NULL, NULL),
+(7, 2, 7, NULL, NULL),
+(8, 9, 8, NULL, NULL),
+(9, 8, 9, NULL, NULL),
+(10, 9, 10, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -223,15 +222,16 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`id`, `created_at`, `updated_at`, `role_id`, `user_id`) VALUES
-(1, NULL, NULL, 2, 1),
+(1, NULL, NULL, 1, 1),
 (2, NULL, NULL, 1, 2),
-(3, NULL, NULL, 2, 3),
+(3, NULL, NULL, 1, 3),
 (4, NULL, NULL, 1, 4),
 (5, NULL, NULL, 2, 5),
-(6, NULL, NULL, 1, 6),
+(6, NULL, NULL, 2, 6),
 (7, NULL, NULL, 2, 7),
-(8, NULL, NULL, 1, 8),
-(10, NULL, NULL, 1, 10),
+(8, NULL, NULL, 2, 8),
+(9, NULL, NULL, 2, 9),
+(10, NULL, NULL, 2, 10),
 (11, NULL, NULL, 1, 11),
 (12, NULL, NULL, 2, 11),
 (13, NULL, NULL, 2, 12),
@@ -261,18 +261,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Zackery Bernhard', 'imorar@example.net', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, '0yHanwKaoO', '2021-11-13 04:36:24', '2021-11-13 04:36:24'),
-(2, 'Giles Farrell MD', 'angel.ebert@example.org', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, '1EDFahoe2W', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(3, 'Miss Lyda Larkin I', 'gwalsh@example.org', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'smaKoRjkgP', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(4, 'Julian Purdy II', 'yschuppe@example.net', '2021-11-13 04:36:24', '$2y$10$lltDe/7DEWOkAOf5MagWYeKqYYaDIqMf6VsK5xptizo39mSMlxtiS', NULL, NULL, 'ZWGXDFeeCFvvcCUkYXrEny5sF6NIuvJsqS8bT556Luu39A4Ih4h492Dfsq60', '2021-11-13 04:36:25', '2021-11-13 04:41:30'),
-(5, 'Loren Bogisich', 'lorenza26@example.com', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, '3lvzE7T6Ar', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(6, 'Trace Lynch', 'rylee27@example.net', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'wkXtSdOHz4', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(7, 'Merle Altenwerth', 'hsauer@example.net', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'KPZcdI2UY1', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(8, 'Maryam Mraz', 'ghuel@example.org', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'qZYBxgwi3Z', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(10, 'Carlee Buckridge', 'hilario.glover@example.com', '2021-11-13 04:36:24', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'DaHu9bCdqw', '2021-11-13 04:36:25', '2021-11-13 04:36:25'),
-(11, 'Norayr Isahakyan', 'norayrisahakyan@gmail.com', '2021-11-13 04:43:21', '$2y$10$FMy9MDu6SqFukC/VHAaP2uEH9.diiHRpX2iUHJ3KmR88PwCPWF5iG', NULL, NULL, 'IIuUIpsdrUhoWA477iEicvSKbdUNcjaPtKeywREVBo9WNUFn7RzL1hPBJd2w', '2021-11-13 04:42:11', '2021-11-13 04:43:21'),
-(12, 'tiop1', 'tiop@ty.ty', '2021-11-13 05:05:56', '$2y$10$Z3WsAvAbNMXqfjSYrrHCT.EcM5.4mjDNwrMi.CBv7DNnHs15hGbve', NULL, NULL, 'EsmEP7atYq3b1c1hpFqqweVA73cMzgtYR0J7Mbw0eYjJlbKBzrVQarHIX0SX', '2021-11-13 04:45:06', '2021-11-13 05:05:56'),
-(13, 'your test', 'y@test.com', NULL, '34343434', NULL, NULL, NULL, '2021-11-13 06:07:31', '2021-11-13 06:07:31');
+(1, 'Prof. Eliezer Dibbert Sr.', 'twilkinson@example.org', '2021-11-16 14:18:53', '$2y$10$rR0qKeCzqgiDr4lgD5uoq.2TnvZj1x7Cvu1wpmIxGyQyMv435FU5a', NULL, NULL, 'dHQh5UYO2XhWqcPwNa4DVlEX2MxAtplzw8J557o2z8ZfY4Xvy55iEW9KnAWJ', '2021-11-16 14:18:53', '2021-11-16 14:20:14'),
+(2, 'Mrs. Jermaine Zemlak II', 'robin.bogan@example.org', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'F2HTEFVzZ8', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(3, 'Lucile Casper', 'nkuhn@example.com', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'T4cpKmr3pe', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(4, 'Earlene Schimmel', 'deanna25@example.com', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'PjCiRCkw0a', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(5, 'Miss Elyssa Kuhlman', 'feeney.zetta@example.net', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'EA05ebcwMM', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(6, 'Sandra Lebsack I', 'eleanora.beatty@example.net', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'CLnnPN0tTm', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(7, 'Jazmyne Bradtke II', 'mgoldner@example.net', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'NKAcp3fxu1', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(8, 'Antwon Gusikowski', 'kariane.pacocha@example.org', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, '6dHticZkG2', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(9, 'Prof. Sylvester Gaylord DVM', 'clare04@example.com', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, 'W8sPG4PEHk', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(10, 'Prof. Anika Kris', 'samara.witting@example.net', '2021-11-16 14:18:53', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, NULL, '2dCQuIjVy2', '2021-11-16 14:18:53', '2021-11-16 14:18:53'),
+(11, 'Norayr Isahakyan', 'norayrisahakyan@gmail.com', '2021-11-16 14:23:18', '$2y$10$c5NpFseZgwKKi9DB35Pt0.kwete3D6enwxtav/XyMLDU7Ir8xpQCG', NULL, NULL, '4N7al0wIUpTIGNiCJruI3PKbu0H3bAKp9n6dYi7iiYRmd3xQpq3wJ7hJ0MD5', '2021-11-16 14:21:19', '2021-11-16 14:23:18'),
+(12, 'Test 1', 'test1@fol.ui', '2021-11-16 14:24:42', '$2y$10$Be01FRPi6rJ1ENwhrN691OlmGdfiwsGz2m4HkDU49nk7h3r9/wvIi', NULL, NULL, 'Q5qvPEst6ixw1rcP6INh96b5uD2wyklWvqWaFrm4FGdSNTVkTuaOl7ZMqxw9', '2021-11-16 14:21:51', '2021-11-16 14:24:42'),
+(13, 'Test 2', 'tetst@gmail.com', '2021-11-16 14:26:15', '$2y$10$zuPXivx1Wubvc0tWY0mHb.6bMuZVMXIGqKvkMlDPKFkqHUfilJUGC', NULL, NULL, 't4LxIm1vMFYNc7KKSoy7lvrkt9cq9oRSmwd5kXsRhK9eVNpVBySWEMTKgwCg', '2021-11-16 14:22:26', '2021-11-16 14:26:15');
 
 --
 -- Indexes for dumped tables
@@ -368,7 +369,7 @@ ALTER TABLE `media`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=297;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -380,7 +381,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `post_user`
